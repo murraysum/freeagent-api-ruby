629d29c1fa577552774788215ccb0116733a587f
Murray Summers <murray.sum@gmail.com>
2012-08-14 16:36:09 +0100

    Fix filter resource method

--- lib/freeagent/resource.rb
+++ lib/freeagent/resource.rb
@@ -113,7 +113,7 @@ module FreeAgent
     def self.define_filter
       self.define_singleton_method(:filter) do |params|
         response = FreeAgent.client.get("#{endpoint[:plural]}/", params)
-        response[:endpoint[:plural]].collect{ |r| self.new(r) }
+        response[endpoint[:plural]].collect{ |r| self.new(r) }
       end
     end
 
