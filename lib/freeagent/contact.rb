4a771694f880e6cbb1c2364503d8b25448fd4afc
Murray Summers <murray.sum@gmail.com>
2012-08-15 09:47:22 +0100

    Add in filter wrappers

--- lib/freeagent/contact.rb
+++ lib/freeagent/contact.rb
@@ -13,5 +13,37 @@ module FreeAgent
     date_accessor :created_at, :updated_at
 
     decimal_accessor :account_balance
+
+    def self.active
+      Contact.filter(:view => 'active')
+    end
+
+    def self.clients
+      Contact.filter(:view => 'clients')
+    end
+
+    def self.suppliers
+      Contact.filter(:view => 'suppliers')
+    end
+
+    def self.active_projects
+      Contact.filter(:view => 'active_projects')
+    end
+
+    def self.completed_projects
+      Contact.filter(:view => 'completed_projects')
+    end
+
+    def self.open_clients
+      Contact.filter(:view => 'open_clients')
+    end
+
+    def self.open_suppliers
+      Contact.filter(:view => 'open_suppliers')
+    end
+
+    def self.hidden
+      Contact.filter(:view => 'hidden')
+    end
   end
 end
