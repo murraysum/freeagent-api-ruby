4882a44e1331ea7a524bea0d71f5ed8813e48a10
Murray Summers <murray.sum@gmail.com>
2012-09-28 21:43:41 +0100

    Fix bank transaction to be in the FreeAgent module

--- lib/freeagent/bank_transaction.rb
+++ lib/freeagent/bank_transaction.rb
@@ -1,34 +1,36 @@
-class BankTransaction < Resource
-  resource :bank_transaction
+module FreeAgent
+  class BankTransaction < Resource
+    resource :bank_transaction
 
-  resource_methods :find, :filter 
+    resource_methods :find, :filter 
 
-  attr_accessor :bank_account, :description, :is_manual, :bank_transactions_explanations
-  decimal_accessor :amount, :unexplained_amount
+    attr_accessor :bank_account, :description, :is_manual, :bank_transactions_explanations
+    decimal_accessor :amount, :unexplained_amount
 
-  date_accessor :dated_on
+    date_accessor :dated_on
 
-  def self.find_all_by_bank_account(bank_account, options = {})
-    options.merge!(:bank_account => bank_account)
-    BankTransaction.filter(options) 
-  end
+    def self.find_all_by_bank_account(bank_account, options = {})
+      options.merge!(:bank_account => bank_account)
+      BankTransaction.filter(options) 
+    end
 
-  def self.unexplained(bank_account, options = {})
-    options.merge!(:view => 'unexplained', :bank_account => bank_account)
-    BankTransaction.filter(options)
-  end
+    def self.unexplained(bank_account, options = {})
+      options.merge!(:view => 'unexplained', :bank_account => bank_account)
+      BankTransaction.filter(options)
+    end
 
-  def self.manual(bank_account, options = {})
-    options.merge!(:view => 'manual', :bank_account => bank_account)
-    BankTransaction.filter(options)
-  end
+    def self.manual(bank_account, options = {})
+      options.merge!(:view => 'manual', :bank_account => bank_account)
+      BankTransaction.filter(options)
+    end
 
-  def self.imported(bank_account, options = {})
-    options.merge!(:view => 'imported', :bank_account => bank_account)
-    BankTransaction.filter(options)
-  end
+    def self.imported(bank_account, options = {})
+      options.merge!(:view => 'imported', :bank_account => bank_account)
+      BankTransaction.filter(options)
+    end
 
-  def self.upload_statement(statement, bank_account)
-    raise FreeAgent::NotImplemented.new("FIXME Implement")
+    def self.upload_statement(statement, bank_account)
+      raise FreeAgent::NotImplemented.new("FIXME Implement")
+    end
   end
 end
