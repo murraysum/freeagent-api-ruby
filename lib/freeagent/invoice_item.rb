a9293e0e7ca6c72e8e26317a78d2b2a5710468fa
Yann Vaillant <yann@vaillant.im>
2015-09-02 12:25:53 +0200

    Add sales_tax_rate to InvoiceItem

--- lib/freeagent/invoice_item.rb
+++ lib/freeagent/invoice_item.rb
@@ -2,6 +2,6 @@ module FreeAgent
   class InvoiceItem < Resource
     attr_accessor :description, :item_type
 
-    decimal_accessor :price, :quantity
+    decimal_accessor :price, :quantity, :sales_tax_rate
   end
 end
